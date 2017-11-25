460a36542480e4ac9204ab87c6691a48fcf5b4c4
James Long <jameslong0410@gmail.com>
2017-11-25 13:20:30 -0500

    Large batch of updates
    Update script to better detect URLs, add units from TW Summer, Konosuba collab, 9M Downloads Event, Material Quest 6*s

--- Compare game8 ratings to xlsx.py
+++ Compare game8 ratings to xlsx.py
@@ -8,7 +8,7 @@ from openpyxl import load_workbook
 wb = load_workbook("Tier List.xlsx")
 
 sheetNames = ["Red", "Blue", "Green", "Yellow"]
-sheetURLs = ["https://game8.jp/archives/40065", "https://game8.jp/archives/40266", "https://game8.jp/archives/40259", "https://game8.jp/archives/40060"]
+sheetURLs = ["https://game8.jp/crashfever/40065", "https://game8.jp/crashfever/40266", "https://game8.jp/crashfever/40259", "https://game8.jp/crashfever/40060"]
 
 for colourNum in range(len(sheetNames)):
 
@@ -18,16 +18,17 @@ for colourNum in range(len(sheetNames)):
      page = requests.get(sheetURLs[colourNum])
      tree = html.fromstring(page.content)
      gameLinks = tree.xpath('//table[2]//td[1]/a/@href')
+     gameLinkNums = [i.split('/')[-1] for i in gameLinks]
      gameNames = tree.xpath('//table[2]//td[1]/a/text()')
      gameRatings = tree.xpath('//table[2]//td[3]/b/text()')
-
      for x in range(sheet.max_row):
           fullName = sheet['A' + str(x+1)].value
           if(fullName.find("https://") >= 0):
                currRating = str(sheet['B' + str(x+1)].value)[:2]
-               linkToMatch = fullName[fullName.find("https://"):-1]
-               for num, link in enumerate(gameLinks):
-                    if(linkToMatch == link):
+               linkToMatch = fullName[fullName.find("https://"):fullName.rfind(")")]
+               linkToMatchNum = linkToMatch.split('/')[-1]
+               for num, link in enumerate(gameLinkNums):
+                    if(linkToMatchNum == link):
                          gameRating = gameRatings[num][:2]
                          if(gameRating.isdigit()):
                               if(gameRating != currRating):
--- Convert xlsx to txt.py
+++ Convert xlsx to txt.py
@@ -12,12 +12,12 @@ for colourNum in range(len(colours)):
      print("Writing " + colours[colourNum] + ", please wait...")
      
      writeTo.write("#" + colours[colourNum] + " Units\n\n")
-     writeTo.write("Name | Rating | Obtain Method | 6* in GL?\n")
-     writeTo.write(":--|:--|:--|:--\n")
+     writeTo.write("Name | Rating | Obtain Method\n")
+     writeTo.write(":--|:--|:--\n")
 
      sheet = wb.get_sheet_by_name(colours[colourNum])
      for x in range(sheet.max_row):
-          writeTo.write(sheet['A' + str(x+1)].value.replace('https', 'http') + " | " + str(sheet['B' + str(x+1)].value) + " | " + sheet['C' + str(x+1)].value + " | " + sheet['D' + str(x+1)].value + "\n")
+          writeTo.write(sheet['A' + str(x+1)].value.replace('https', 'http') + " | " + str(sheet['B' + str(x+1)].value) + " | " + sheet['C' + str(x+1)].value + "\n")
      writeTo.write("\n")
 
 writeTo.close()
